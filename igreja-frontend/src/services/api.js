b6e0459b9290dafb462886300f6a9c858b182cf4
carlosjeferson <Carlosjjs500214@gmail.com>
2026-02-14 01:07:52 -0300

    atualizando

--- igreja-frontend/src/services/api.js
+++ igreja-frontend/src/services/api.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000', // Porta que definimos no server.ts
+  baseURL: import.meta.env.VITE_API_URL, 
 });
 
-// Interceptor para enviar o token JWT automaticamente em todas as requisições
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('@ADTC:token');
   if (token) {
